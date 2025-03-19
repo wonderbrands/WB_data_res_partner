0a0410bad7e20f11be73cea6598f2d0c42a867ec
demianavilar@gmail.com <Demian Avila>
2025-03-19 14:25:53 +0100

    feat: adapted to v18, installable

--- __manifest__.py
+++ __manifest__.py
@@ -18,28 +18,17 @@
     'author': "Wonderbrands",
     'website': "https://www.wonderbrands.co",
     'license': 'LGPL-3',
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Inventory',
-    'version': '15.0',
+    'version': '18.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base',
                 'product',
                 'sale',
-                'stock'],
+                'stock',
+                ],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/res_partner_views.xml',
-        'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+   
 }
--- models/res_partner.py
+++ models/res_partner.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-import base64
-from odoo import api, fields, models, SUPERUSER_ID
-from odoo import models, fields, api, _
-from odoo.exceptions import Warning
-from datetime import datetime
-import logging
-import json
-import requests
-from io import StringIO, BytesIO
+from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
